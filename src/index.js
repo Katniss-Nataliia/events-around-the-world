92283f72930113a820bc9ba4f5c8e8011a530c52
Katniss-Nataliia <natko.boiko@gmail.com>
2023-07-03 23:08:26 -0600

    added code

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// API Key and Endpoint
+
+const API_KEY = '6SYmqFz6xiK0U5ewaGGoNFKniviJRYgX';
+const ENDPOINT = 'https://app.ticketmaster.com/discovery/v2/';
+
+const form = document.querySelector('.search-form');
+let currentSearchQuery = '';
+
+
+const gallery = document.querySelector('.gallery');
