c7fda413b84cae6a04c24632b2cfbaf60e0b9870
Katniss-Nataliia <natko.boiko@gmail.com>
2023-07-03 23:42:04 -0600

    added code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-// API Key and Endpoint
+// 1 - API Key and Endpoint
 
 const API_KEY = '6SYmqFz6xiK0U5ewaGGoNFKniviJRYgX';
 const ENDPOINT = 'https://app.ticketmaster.com/discovery/v2/';
 
+
+// 2 - Selecting DOM elements
+
 const form = document.querySelector('.search-form');
-let currentSearchQuery = '';
-
-
 const gallery = document.querySelector('.gallery');
+
+let currentSearchQuery = '';
+let currentPage = 1;
+let totalHits = 0; //The total number of search results
+
+
+// 3 - event listener for the form
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    currentSearchQuery = document.querySelector('input[name="searchQuery"]').value;
+    apiFetchEvents(currentSearchQuery); //Fetch images based on the search query (function in step 4)
+})
+
+// 4 - Function to fetch images from the ticketmaster API
+
+function apiFetchEvents (query){
+    const eventURL = `${ENDPOINT}events.json?&keyword=${query}&countryCode=US&apikey=${API_KEY}`
+}
